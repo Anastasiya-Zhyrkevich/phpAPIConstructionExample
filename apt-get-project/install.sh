0020e0bbcd75d2072329163f74b215640fbd08a4
Anastasiya-Zhyrkevich <Anastasiya.Zhyrkevich@yandex.ru>
2021-03-03 14:21:42 +0300

    clean up

--- apt-get-project/install.sh
+++ apt-get-project/install.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-mkdir ./bin
-
-# wget -O phpunit https://phar.phpunit.de/phpunit-9.phar \
-#  && chmod +x phpunit \
-#  && sudo mv phpunit /usr/local/bin/phpunit
-
-# sudo apt-get install -y php-guzzle
-# sudo apt-get install -y --no-install-recommends php-symfony-process
-
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
   && php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" \
   && php composer-setup.php --filename=composer \
@@ -18,8 +9,9 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
 ./composer global require symfony/process=3.3 
 ./composer global require guzzlehttp/guzzle=6.3
 
-sudo ln -s ~/.config/composer/vendor/bin/phpunit phpunit
+COMPOSER_HOME_PATH=$(./composer global config bin-dir --absolute)
+sudo ln -s ${COMPOSER_HOME_PATH}/phpunit phpunit
  	
-export PATH=~/.config/composer/vendor/bin:$PATH
+export PATH=${COMPOSER_HOME_PATH}:$PATH
 
 rm -rf composer
